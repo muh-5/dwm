f60eefb7c99d9669ef1df5e737e9975336b078d3
muhammed <malhile5@gmail.com>
2025-06-13 18:26:25 +0300

    use only fibonacci as mode

--- config.def.h
+++ config.def.h
@@ -40,11 +40,15 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+ 	{ "[@]",      spiral }, /* first entry is default */
+	/* { "[]=",      tile }, */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
+	/*
+	 * use only fibanocci as mode 
+	 * { "[M]",      monocle },
+	 * { "[@]",      spiral },
+	 * { "[\\]",      dwindle },
+	 */
 };
 
 /* key definitions */
