e86ec25f6cf760ab019adb5851e636969d3f5b1a
muhammed <malhile5@gmail.com>
2025-06-25 11:57:04 +0300

    some personal config

--- config.def.h
+++ config.def.h
@@ -105,7 +105,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Right,  viewnext,       {0} },
 	{ MODKEY,                       XK_Left,   viewprev,       {0} },
 	{ MODKEY|ShiftMask,             XK_Right,  tagtonext,      {0} },
+	{ SUPER, 		       	XK_Right,  tagtonext,      {0} },
 	{ MODKEY|ShiftMask,             XK_Left,   tagtoprev,      {0} },
+	{ SUPER,             		XK_Left,   tagtoprev,      {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
